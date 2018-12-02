f73f003ec443f688ba3f940109d703eee110f1d7
AnjuAshok <anjuashok000@gmail.com>
2018-12-02 12:58:16 +0530

    corrected the code

--- main.go
+++ main.go
@@ -1,28 +1,30 @@
 package main
 import "fmt"
 
-func sort (x []int) []int{
-
-        for i:=0; i < l; i++{
-                for j:=i+1 ;j<l; j++{
-                        if x[i] >x[j]{
-                        a := x[i]
-                        x[i]=x[j]
-                        x[j] = a
-                        }
-                }
-	return x[i]
+func sort(x []int) []int{
+  for i := 0; i < len(x); i++ {
+    for j := i+1; j<len(x); j++ {
+      if x[i] > x[j]{
+        a := x[i]
+        x[i] = x[j]
+        x[j] = a
+      }
+    }
+  }
+	return x
 }
+
 func main() {
-	var y := make([]int,5)
-	l := len(y)
-         for i:=0; i < l ;i++{
-                fmt.Scanln(&y[i])
-        }
+  var y []int
+  var l int
+  fmt.Println("Plerase enter the size of array: ")
+  fmt.Scanln(&l)
+  fmt.Println("Enter the values: ")
+  for i:=0; i < l; i++ {
+      var temp int
+      fmt.Scanln(&temp)
+      y = append(y, temp)
+  }
 	new := sort(y)
 	fmt.Println("The sorted slice is :", new)
-
-
-
-
 }
